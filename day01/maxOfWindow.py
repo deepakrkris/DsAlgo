5d25838045c6d705a6913a1fbcb9d512cb8903ff
Deepak Rajamohan <thangarajamohan@Deepaks-MacBook-Pro.local>
2022-10-29 23:10:35 -0700

    final max sliding window

--- day01/maxOfWindow.py
+++ day01/maxOfWindow.py
@@ -16,15 +16,40 @@ class Solution:
 
         return slidingMax
 
+    def maxWindowV2(self, nums: List[int], size) -> int:
+        window = []
+
+        if size > len(nums):
+            size = len(nums)
+
+        q = deque()
+
+        for index in range(size) :
+            while q and nums[index] > nums[q[-1]] :
+                q.pop()
+            q.append(index)
+
+        window.append(nums[q[0]])
+        
+        for index in range(size, len(nums)) :
+            while q and nums[index] > nums[q[-1]] :
+                q.pop()
+        
+            if q and q[0] <= (index - size) :
+                q.popleft()
+
+            q.append(index)
+            window.append(nums[q[0]])
+        
+        return window
+    
+    def callFn(self, nums: List[int], windowSize) -> int:
+        return self.maxWindowV2(nums, windowSize)
 
 S = Solution()
 
-# print(S.optimized([4, 5, 6, 1, 2, 3] , 1))
-print(S.maxOfWindow([3, 2, 1, 1, 0], 3))
-# , S.optimized([3, 2, 1, 1, 0], 3))
-print(S.maxOfWindow([3, 4, 2, 4, 3, 5], 4))
-# , S.optimized([3, 4, 2, 4, 3, 5], 4))
-print(S.maxOfWindow([3, 4, 2, 4, 3, 5], 8))
-# , S.optimized([3, 4, 2, 4, 3, 5], 8))
-print(S.maxOfWindow([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] , 3))
-# , S.optimized([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] , 3))
+print(S.callFn([4, 5, 6, 1, 2, 3] , 1))
+print(S.callFn([3, 2, 1, 1, 0], 3))
+print(S.callFn([3, 4, 2, 4, 3, 5], 4))
+print(S.callFn([3, 4, 2, 4, 3, 5], 8))
+print(S.callFn([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] , 3))
